ab2aa65ce2fc1631f473f38ed896ae752d0dce2b
PavelFomkin <pavel_foma@mail.ru>
2019-12-19 13:04:48 +0300

    Merge branch 'dev' of C:\Users\UC248557\IdeaProjects\JetStream\AngularMentoringApp with conflicts.

--- src/app/courses-module/components/courses/courses.component.ts
+++ src/app/courses-module/components/courses/courses.component.ts
@@ -1,67 +1,89 @@
-import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Course} from '../../models/course';
 import {BreadcrumbLink} from '../../../shared-module/models/breadcrumb-link';
-import {CourseService} from '../../services/course.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {distinctUntilChanged, filter, mergeAll, switchMap, throttleTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit, OnChanges {
+export class CoursesComponent implements OnInit {
 
+  searchRequest = new Subject();
+
+  numberOfCoursesToLoad: number = 5;
+  hasMoreCourses: boolean = false;
   noCoursesTitle: string = 'Courses not found';
   searchData: string;
-  availableNumberOfCourses: number;
   loadedCourses: Course[];
   breadcrumbLinks: BreadcrumbLink[] = [
-      {title: 'Courses', url: '/courses'}
-    ];
+    {title: 'Courses', url: '/courses'}
+  ];
 
-  constructor(private courseService: CourseService) {
-    console.log('created a new courses component');
+  constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit courses component');
-    this.availableNumberOfCourses = this.courseService.getNumberOfCourses();
-    this.loadedCourses = this.availableNumberOfCourses > 0 ? this.courseService.getCourses(0) : [];
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges courses component');
-    console.log(changes);
+    this.httpClient.get<Course[]>('http://localhost:3004/courses?start=0&count=' + this.numberOfCoursesToLoad)
+      .subscribe((items: Course[]) => {
+        this.loadedCourses = items;
+        this.hasMoreCourses = this.numberOfCoursesToLoad <= this.loadedCourses.length;
+      });
   }
 
   removeCourse(id: number) {
     if (confirm('Do you really want to delete this course?')) {
-      this.courseService.removeCourse(id);
-      this.loadedCourses = this.loadedCourses.filter(course => id !== course.id);
-      this.availableNumberOfCourses = this.searchData ?
-        this.courseService.getNumberOfCourses(this.searchData) :
-        this.courseService.getNumberOfCourses();
+      this.httpClient.delete<void>('http://localhost:3004/courses' + id)
+        .subscribe(() => {
+          this.loadedCourses = this.loadedCourses.filter(item => item.id !== id);
+        });
     }
   }
 
-  searchCourses(searchData?: string) {
-    if (searchData) {
-      this.searchData = searchData.trim();
-      this.availableNumberOfCourses = this.courseService.getNumberOfCourses(this.searchData);
-      this.loadedCourses = this.availableNumberOfCourses > 0 ?
-        this.courseService.getCourses(0, this.searchData) : [];
-    } else {
-      this.searchData = undefined;
-      this.availableNumberOfCourses = this.courseService.getNumberOfCourses();
-      this.loadedCourses = this.availableNumberOfCourses > 0 ?
-        this.courseService.getCourses(0) : [];
-    }
+  searchCourses(searchData: string) {
+    this.searchRequest.next(searchData);
+
+    // this.loadedCourses = [];
+    this.searchData = searchData ? searchData.trim() : '';
+
+    this.searchRequest.pipe(
+      filter((text: string) => !!text || text.length >= 3),
+      distinctUntilChanged(),
+      throttleTime(250),
+      switchMap(() =>
+        this.httpClient.get<Course[]>('http://localhost:3004/courses?start=0&count=' + this.numberOfCoursesToLoad
+        + '&textFragment=' + this.searchData)),
+    ).subscribe((items: Course[]) => {
+      this.loadedCourses = items;
+      this.hasMoreCourses = this.numberOfCoursesToLoad <= this.loadedCourses.length;
+    });
+
+    // this.httpClient.get<Course[]>('http://localhost:3004/courses?start=0&count=' + this.numberOfCoursesToLoad
+    //   + '&textFragment=' + this.searchData)
+    //   .subscribe((items: Course[]) => {
+    //     this.loadedCourses = items;
+    //     this.hasMoreCourses = this.numberOfCoursesToLoad <= this.loadedCourses.length;
+    //   });
   }
 
   loadMore() {
+    let attributes;
     if (this.searchData) {
-      this.loadedCourses = [...this.loadedCourses, ...this.courseService.getCourses(this.loadedCourses.length, this.searchData)];
+      attributes = 'start=' + this.loadedCourses.length
+        + '&count=' + this.numberOfCoursesToLoad
+        + '&textFragment=' + this.searchData;
     } else {
-      this.loadedCourses = [...this.loadedCourses, ...this.courseService.getCourses(this.loadedCourses.length)];
+      attributes = 'start=' + this.loadedCourses.length
+        + '&count=' + this.numberOfCoursesToLoad;
     }
+    this.httpClient.get<Course[]>('http://localhost:3004/courses?' + attributes)
+      .subscribe((items: Course[]) => {
+        const expectedCountOfCourses = this.loadedCourses.length + this.numberOfCoursesToLoad;
+        this.loadedCourses = [...this.loadedCourses, ...items];
+        this.hasMoreCourses = expectedCountOfCourses <= this.loadedCourses.length;
+      });
   }
 }
